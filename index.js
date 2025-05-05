33b7c20c70dc2f33557ee9380c061d6837f3619b
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-05 15:38:53 -0400

    Pass all tests for first function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400) return "This one is on me!";
+  else if (someNumber > 400 && someNumber < 2000)
+    return "That will be twenty bucks.";
+  else if (someNumber > 2000 && someNumber <= 2500)
+    return "I will gladly take your thirty bucks.";
+  else if (someNumber > 2500) return "No can do.";
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
