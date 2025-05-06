e1e91e2fa2cb3c03fd958a2384b1b1934d361762
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-06 09:54:12 -0400

    Refactor scuberGreetingForFeet

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 function scuberGreetingForFeet(someNumber) {
   if (someNumber <= 400) return "This one is on me!";
-  else if (someNumber > 400 && someNumber < 2000)
-    return "That will be twenty bucks.";
-  else if (someNumber > 2000 && someNumber <= 2500)
-    return "I will gladly take your thirty bucks.";
+  else if (someNumber <= 2000) return "That will be twenty bucks.";
+  else if (someNumber <= 2500) return "I will gladly take your thirty bucks.";
   else return "No can do.";
-}
-
-function ternaryCheckCity(someCity) {
-  if (someCity === "NYC") return "Ok, sounds good.";
-  else return "No go.";
 }
 
 function ternaryCheckCity(someCity) {
