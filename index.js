ad5dac141b214d1cb14437845fb65a148fbb6769
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-06 09:47:51 -0400

    Refactor 'No go.' to else instead of else if

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function scuberGreetingForFeet(someNumber) {
     return "That will be twenty bucks.";
   else if (someNumber > 2000 && someNumber <= 2500)
     return "I will gladly take your thirty bucks.";
-  else if (someNumber > 2500) return "No can do.";
+  else return "No can do.";
 }
 
 function ternaryCheckCity(someCity) {
   if (someCity === "NYC") return "Ok, sounds good.";
-  else if (someCity !== "NYC") return "No go.";
+  else return "No go.";
 }
 
 function ternaryCheckCity(someCity) {
